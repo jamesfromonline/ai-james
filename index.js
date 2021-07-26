63ffa07fde529281f4d284f0ca632200cca47dbd
jamesfromonline <jtwebco@gmail.com>
2021-07-26 14:33:13 -0400

    adds more logs

--- index.js
+++ index.js
@@ -25,7 +25,10 @@ const handleSheetsUpdate = async () => {
     if (nextTweet)
       T.post('statuses/update',
       { status: nextTweet },
-      async err => !err && await rows[0].delete())
+      async err => {
+        !err && await rows[0].delete()
+        err && console.log(err)
+      })
       console.log(new Date(), nextTweet)
   } catch (e) {
     throw e.message
