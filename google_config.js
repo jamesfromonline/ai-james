bc75fc247e0e65b831358305d09ad5001ea0ce38
jamesfromonline <jtwebco@gmail.com>
2021-07-26 10:07:23 -0400

    private key fix attempt

--- google_config.js
+++ google_config.js
@@ -4,7 +4,7 @@ module.exports = {
   "type": "service_account",
   "project_id": "mepage",
   "private_key_id": process.env.GOOGLE_PRIVATE_KEY_ID,
-  "private_key": process.env.GOOGLE_PRIVATE_KEY,
+  "private_key": process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n'),
   "client_email": process.env.GOOGLE_EMAIL,
   "client_id": process.env.GOOGLE_CLIENT_ID,
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
